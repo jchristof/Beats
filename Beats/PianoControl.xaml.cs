ae73df033bf11bf75e7d60b787c143eb56cbfc4b
jchri <jchri@DESKTOP-BMST19G>
2017-09-06 19:05:48 -0500

    adding piano control

---
+++ Beats/PianoControl.xaml.cs
@@ -0,0 +1,24 @@
+﻿
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+
+namespace Beats
+{
+    public sealed partial class PianoControl : UserControl
+    {
+        public PianoControl()
+        {
+            this.InitializeComponent();
+        }
+
+        private void WhiteKey0_PointerPressPreview(object sender, System.EventArgs e)
+        {
+
+        }
+
+        private void WhiteKey0_PointerReleasedPreview(object sender, System.EventArgs e)
+        {
+
+        }
+    }
+}
