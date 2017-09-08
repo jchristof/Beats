150b09d3cd69540e5988ff99523330e333125279
Jon Martin <jchristofmartin@gmail.com>
2017-09-07 20:17:26 -0500

    cut down the keyboard xaml to one octave

--- Beats/FrameNode.cs
+++ Beats/FrameNode.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Media;
 using Windows.Media.Audio;
