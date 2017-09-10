39bb5b2022a90f1e21aca4b5740dafc707b81195
Jon Martin <jchristofmartin@gmail.com>
2017-09-10 09:46:35 -0500

    added note names to midi note numbers table. hooked up sending note on and off commands.

--- Beats/PianoToMidiOut.xaml.cs
+++ Beats/PianoToMidiOut.xaml.cs
@@ -1,17 +1,36 @@
 ﻿using System;
-using Windows.UI.Xaml.Controls;
+using System.Collections.Generic;
+using Windows.Devices.Midi;
 
-namespace Beats
-{
-    public sealed partial class PianoToMidiOut : UserControl
-    {
-        public PianoToMidiOut()
-        {
-            this.InitializeComponent();
+namespace Beats {
+    public sealed partial class PianoToMidiOut {
+
+        public PianoToMidiOut() {
+            InitializeComponent();
         }
 
+        private static Dictionary<string, byte> NoteToMidi = new Dictionary<string, byte> {
+            {"C", 60},
+            {"Cs", 61},
+            {"D", 62},
+            {"Ds", 63},
+            {"E", 64},
+            {"F", 65},
+            {"Fs", 66},
+            {"G", 67},
+            {"Gs", 68},
+            {"A", 69},
+            {"As", 70},
+            {"B", 71},
+            
+        };
+
         private void PianoControl_OnKeyPressedEvent(object sender, EventArgs e) {
-            MidiSelector.SendOut(0, 60, 127);
+            MidiSelector.SendOut(0, NoteToMidi[(sender as AudioButton).Name], 127, MidiMessageType.NoteOn);
+        }
+
+        private void PianoControl_OnKeyReleasedEvent(object sender, EventArgs e) {
+            MidiSelector.SendOut(0, NoteToMidi[(sender as AudioButton).Name], 127, MidiMessageType.NoteOff);
         }
 
     }
