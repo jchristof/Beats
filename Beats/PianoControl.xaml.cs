f3cb0b4742340fe49363078d9840ce426a13a67d
Jon Martin <jchristofmartin@gmail.com>
2017-09-10 09:44:47 -0500

    add key press and release events to each key and an event for each type. added note names corresponding to each key.

--- Beats/PianoControl.xaml.cs
+++ Beats/PianoControl.xaml.cs
@@ -1,28 +1,26 @@
 ﻿
 using System;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Input;
 
 namespace Beats
 {
-    public sealed partial class PianoControl : UserControl
+    public sealed partial class PianoControl
     {
         public PianoControl()
         {
             this.InitializeComponent();
         }
 
-        private void WhiteKey0_PointerPressPreview(object sender, System.EventArgs e)
+        private void PointerPressPreview(object sender, System.EventArgs e)
         {
             KeyPressedEvent.Invoke(sender, e);
         }
 
-        private void WhiteKey0_PointerReleasedPreview(object sender, System.EventArgs e)
+        private void PointerReleasedPreview(object sender, System.EventArgs e)
         {
-
+            KeyReleasedEvent.Invoke(sender, e);
         }
 
         public event EventHandler KeyPressedEvent = delegate { };
-
+        public event EventHandler KeyReleasedEvent = delegate { };
     }
 }
