44f74e4c5fb40384729b0611e0df4170c150a466
Jon Martin <jchristofmartin@gmail.com>
2017-10-01 21:03:11 -0500

    code cleanup. changing program on mini out. new project for custom controls.

---
+++ Controls/IntPicker.xaml.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+
+namespace Controls {
+    public sealed partial class IntPicker : UserControl {
+
+        public event EventHandler<int> ValueChanged = delegate { };
+        public IntPicker() {
+            InitializeComponent();
+        }
+        private void Reduce_Tapped(object sender, TappedRoutedEventArgs e) {
+            var currentValue = int.Parse(content.Text);
+
+            if (currentValue == 0) {
+                return;
+            }
+            content.Text = (currentValue - 1).ToString();
+            ValueChanged.Invoke(this, currentValue);
+        }
+
+        private void Increase_Tapped(object sender, TappedRoutedEventArgs e) {
+            var currentValue = int.Parse(content.Text);
+            content.Text = (currentValue + 1).ToString();
+            ValueChanged.Invoke(this, currentValue);
+        }
+    }
+}
--- MidiInOut/MainPage.xaml.cs
+++ MidiInOut/MainPage.xaml.cs
@@ -1,15 +1,19 @@
 ﻿
 using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
 using Windows.ApplicationModel.Core;
 using Windows.Devices.Enumeration;
 using Windows.Devices.Midi;
 using Windows.UI.Core;
+using MidiInOut.Annotations;
 
 namespace MidiInOut {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage {
+    public sealed partial class MainPage : INotifyPropertyChanged{
         public MainPage() {
             InitializeComponent();
         }
@@ -30,7 +34,6 @@ namespace MidiInOut {
         private async void MidiOutSelector_OnDeviceSelectedEvent(object sender, DeviceInformation e) {
 
             midiOutPort = await MidiOutPort.FromIdAsync(e.Id);
-
             if (midiOutPort == null) {
                 System.Diagnostics.Debug.WriteLine("Unable to create MidiOutPort from output device");
             }
@@ -38,22 +41,54 @@ namespace MidiInOut {
 
         private async void MidiInPort_MessageReceived(MidiInPort sender, MidiMessageReceivedEventArgs args) {
             midiOutPort?.SendMessage(args.Message);
+
+            if(!logCommands)
+                return;
             await CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(
                 CoreDispatcherPriority.Normal,
                     () => {
                         IMidiMessage receivedMidiMessage = args.Message;
 
                         System.Diagnostics.Debug.WriteLine(receivedMidiMessage.Timestamp.ToString());
-
+                        StringBuilder builder = new StringBuilder();
+                        builder.Append(receivedMidiMessage.Type);
                         if (receivedMidiMessage.Type == MidiMessageType.NoteOn) {
                             MidiNoteOnMessage noteOnMessage = (MidiNoteOnMessage)receivedMidiMessage;
-                            TextBox.Text = $"Note on {noteOnMessage.Channel} {noteOnMessage.Note} {noteOnMessage.Velocity}" + Environment.NewLine + TextBox.Text;
+                            builder.Append($"Note on {noteOnMessage.Channel} {noteOnMessage.Note} {noteOnMessage.Velocity}");
+                            
                         }
                         else if (receivedMidiMessage.Type == MidiMessageType.NoteOff) {
                             MidiNoteOffMessage noteOffMessage = (MidiNoteOffMessage)receivedMidiMessage;
-                            TextBox.Text = $"Note off {noteOffMessage.Channel} {noteOffMessage.Note} " + Environment.NewLine + TextBox.Text;
+                            builder.Append($"Note off {noteOffMessage.Channel} {noteOffMessage.Note} ");
                         }
+
+                        TextBox.Text = builder + Environment.NewLine + TextBox.Text;
                     });
         }
+
+        private bool logCommands;
+
+        public bool LogCommands {
+            get => logCommands;
+            set {
+                if (logCommands == value)
+                    return;
+
+                logCommands = value;
+                RaisePropertyChanged();
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        private void RaisePropertyChanged([CallerMemberName] string propertyName = null) {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void IntPicker_OnValueChanged(object sender, int e) {
+            var m = new MidiProgramChangeMessage(0, (byte)e);
+            midiOutPort?.SendMessage(m);
+        }
+
     }
 }
