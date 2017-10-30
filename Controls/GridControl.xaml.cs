30e3326de882ce3ad2cb4bfb83d106a3679137d6
Jon Martin <jchristofmartin@gmail.com>
2017-10-29 20:30:44 -0500

    adding configurable grid control

---
+++ Controls/GridControl.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Controls {
+    public sealed partial class GridControl {
+        public GridControl() {
+            InitializeComponent();
+            var rowDef = new RowDefinition();
+            rowDef.Height = new GridLength(64);
+
+            rowDef = new RowDefinition {Height = new GridLength(64)};
+
+            Grid.RowDefinitions.Add(rowDef);
+            rowDef = new RowDefinition { Height = new GridLength(64) };
+
+            Grid.RowDefinitions.Add(rowDef);
+
+            var colDef = new ColumnDefinition();
+            colDef.Width = new GridLength(64);
+            Grid.ColumnDefinitions.Add(colDef);
+
+            colDef.Width = new GridLength(64);
+            Grid.ColumnDefinitions.Add(colDef);
+        }
+    }
+}
